8eb91a705f84eee252936e700eea9f7982c24c7a
Pedro Freitas <pedro.hnq.fraga@gmail.com>
2020-01-31 11:05:16 -0300

    Hangman

---
+++ main.py
@@ -0,0 +1,227 @@
+# Hangman Game (Jogo da Forca)
+# Programação Orientada a Objetos
+
+# Import
+import random
+
+# Board (tabuleiro)
+board = ['''
+
+\033[1;37m>>>>>>>>>>Hangman<<<<<<<<<<
+
++---+
+|   |
+    |
+    |
+    |
+    |
+=========''', '''
+
++---+
+|   |
+O   |
+    |
+    |
+    |
+=========''', '''
+
++---+
+|   |
+O   |
+|   |
+    |
+    |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|   |
+     |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+     |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+/    |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+/ \  |
+     |
+=========''']
+
+# Classe
+class Hangman:
+
+	# Método Construtor
+	def __init__(self, word):
+		self.word = word
+		
+	# Método para adivinhar a letra
+	def guess(self, letter):
+		self.letter = letter
+
+	# Método para verificar se o jogo terminou
+	def hangman_over(self, contador):
+		self.contador = contador
+		if self.contador < 6:
+			return False
+		else:
+			return True	
+		
+	# Método para verificar se o jogador venceu
+	def hangman_won(self, certas):
+		self.certas = certas
+
+		if self.certas == len(self.word):
+			return True
+	
+	# Método para checar o status do game e imprimir o board na tela
+	def print_game_status(self, contador):
+		self.contador = contador
+		print(board[self.contador])	
+
+# Função para ler uma palavra de forma aleatória do banco de palavras
+def rand_word(sorteado):
+
+	if(sorteado == 0):
+		with open("arquivos/bandas.txt", "rt") as f:
+			bank = f.readlines()
+		return bank[random.randint(0,len(bank))].upper().strip()
+	elif sorteado == 1:
+		with open("arquivos/comidas.txt", "rt") as f:
+			bank = f.readlines()
+		return bank[random.randint(0,len(bank))].upper().strip()
+	elif sorteado == 2:
+		with open("arquivos/cores.txt", "rt") as f:
+			bank = f.readlines()
+		return bank[random.randint(0,len(bank))].upper().strip()
+	elif sorteado == 3:
+		with open("arquivos/livros.txt", "rt") as f:
+			bank = f.readlines()
+		return bank[random.randint(0,len(bank))].upper().strip()
+	else:
+		with open("arquivos/times.txt", "rt") as f:
+			bank = f.readlines()
+		return bank[random.randint(0,len(bank))].upper().strip()
+
+# Função Main - Execução do Programa
+def main():
+
+	resposta = 'S'
+	while (resposta == 'S'):
+
+		sorteado = random.randint(0, 4)
+
+		# Objeto
+		game = Hangman(rand_word(sorteado))
+
+		corretas = []
+		erradas = []
+		count = 0
+		total = game.word.count(' ')
+
+		dicas = ['Banda/Cantor(a)', 'Comidas Típicas do Brasil', 'Cores', 'Livros', 'Times de Futebol']
+
+
+		# Enquanto o jogo não tiver terminado, print do status, solicita uma letra e faz a leitura do caracter
+
+		while(True):
+			# Verifica o status do jogo
+			game.print_game_status(count)
+
+			print(f'DICA: \033[1;32m{dicas[sorteado]}\033[1;37m')
+			print('Palavra: ', end = '')
+			if len(corretas) == 0 and len(erradas) == 0:
+				for w in range(0,len(game.word)):
+					if (game.word[w] == ' '):
+						print(' ', end='')
+					else:
+						print('_ ', end = '')
+
+				print('\n\nLetras Corretas:\nLetras Erradas: ')
+			else:
+				for w in range(0,len(game.word)):
+					if(game.word[w] == ' '):
+						print(' ', end='')
+					elif(game.word[w] in corretas):
+						print(f'{game.word[w]} ', end='')
+					else:
+						print('_ ', end='')
+					
+
+				print('\n\nLetras Corretas: ', end='')
+				for c in corretas:
+					print(f'{c} ', end='')
+
+				print('\nLetras Erradas: ', end='')
+				for e in erradas:
+					print(f'{e} ', end='')
+			print()
+
+			if(game.hangman_over(count)):
+				break
+			if(game.hangman_won(total)):
+				break
+
+			game.guess(str(input('\nDigite uma letra: ')).upper())
+
+			if (len(game.letter) > 1):
+				print('\033[1;33mVocê só pode digitar uma LETRA por vez.\033[1;37m')
+			elif (len(game.letter) <= 0):
+				print('\033[1;33mVocê DEVE informar pelo menos uma LETRA.\033[1;37m')
+			else:
+				if(len(corretas) == 0 and len(erradas) == 0):
+					if(game.letter in game.word):
+						corretas.append(game.letter)
+						total += game.word.count(game.letter)
+						print(f'\033[1;35mMuito bem. A palavra possui a letra {game.letter}.\033[1;37m')
+					else:
+						erradas.append(game.letter)
+						count += 1
+						print(f'\033[1;31mQue pena. A palavra NÃO possui a letra {game.letter}.\033[1;37m')
+				else:
+					if ((game.letter in corretas) or (game.letter in erradas)):
+						print(f'\033[1;36m\nVocê já tentou essa letra antes.\033[1;37m')
+					else:
+						if(game.letter in game.word):
+							corretas.append(game.letter)
+							total += game.word.count(game.letter)
+							print(f'\033[1;35mMuito bem. A palavra possui a letra {game.letter}.\033[1;37m')
+						else:
+							erradas.append(game.letter)
+							count += 1
+							print(f'\033[1;31mQue pena. A palavra NÃO possui a letra {game.letter}.\033[1;37m')
+			
+		# De acordo com o status, imprime mensagem na tela para o usuário
+		if (game.hangman_won(total)):
+			print ('\n\033[1;93mParabéns! Você venceu!!\033[1;37m')
+
+			resposta = str(input('\033[1;36mDeseja jogar novamente? [S/N] \033[1;37m')).upper()
+
+		elif(game.hangman_over(count)):
+			print ('\n\033[1;95mGame over! Você perdeu.\033[1;37m')
+			print (f'A palavra era \033[1;33m{game.word}\033[1;37m')
+
+			resposta = str(input('\033[1;36mDeseja jogar novamente? [S/N] \033[1;37m')).upper()
+				
+	print ('\nÓtimo jogo. Até a próxima!\n')
+
+# Executa o programa		
+if __name__ == "__main__":
+	main()
